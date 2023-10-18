e95bc6f5f027c68f246def37029f6fb78bd76a5b
gustavofmota <gustavomota.work@gmail.com>
2023-10-18 23:49:01 +0100

    A simple to-do list to understand react hooks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from 'react'
+import './App.css'
+
+let array = [
+	{ name: 'Do Bed', notDone: true },
+	{ name: 'Clean Bedroom', notDone: true },
+	{ name: 'Buy Groceries', notDone: true },
+	{ name: 'Go to the Gym', notDone: true },
+]
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [list, setList] = useState(array)
+	const trRefs = useRef([])
+
+	function hideChecked(index) {
+		const newList = [...list]
+		newList[index].notDone = !newList[index].notDone
+		const element = trRefs.current[index]
+		!newList[index].notDone
+			? (element.style.textDecoration = 'line-through')
+			: (element.style.textDecoration = 'initial')
+		setList(newList)
+	}
+
+	return (
+		<div className='App'>
+			<table>
+				<tbody>
+					{list.map(({ name, notDone }, index) => (
+						<tr
+							key={index}
+							ref={(el) => (trRefs.current[index] = el)}
+						>
+							<td>
+								<input
+									type='checkbox'
+									checked={notDone}
+									onClick={(e) => hideChecked(index)}
+								/>
+							</td>
+							<td>
+								<label htmlFor=''>{name}</label>
+							</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+		</div>
+	)
 }
 
-export default App;
+export default App
